502bed37d7b026dff68d454ebd6e0572192d1f52
ANYuu531 <ANYuu531@gmail.com>
2025-08-05 15:08:22 +0000

    CI/CD test

---
+++ tests/test_main.py
@@ -0,0 +1,16 @@
+import pytest
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from app.main import app
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data == b"Hello, Argo Workflows CI/CD!!!"
